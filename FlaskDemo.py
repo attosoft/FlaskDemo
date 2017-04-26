9b5469ce0c3043540784a666825af48f7e992230
attosoft <420098712@qq.com>
2017-04-26 22:25:31 +0800

    3e

--- FlaskDemo.py
+++ FlaskDemo.py
@@ -1,16 +1,19 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time = datetime.utcnow())
 
 
 @app.route('/user/<name>')
