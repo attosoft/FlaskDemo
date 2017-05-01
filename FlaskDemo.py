423e43d9a00fa8f0d7aa3875c9b3f2d4b6990d0a
attosoft <420098712@qq.com>
2017-05-01 12:43:29 +0800

    4c

--- FlaskDemo.py
+++ FlaskDemo.py
@@ -1,19 +1,37 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(FlaskForm):
+    name  = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'attosoft'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time = datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',
+                           current_time = datetime.utcnow(),
+                           form = form,
+                           name  = session.get('name'))
 
 
 @app.route('/user/<name>')
